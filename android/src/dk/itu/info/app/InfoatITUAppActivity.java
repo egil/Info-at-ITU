720519ebe1cb7d75d0d9e35829209f89af7fa59d
Egil Hansen <egil@assimilated.dk>
2012-03-14 14:16:28 +0100

    auth against info@itu proxy

--- android/src/dk/itu/info/app/InfoatITUAppActivity.java
+++ android/src/dk/itu/info/app/InfoatITUAppActivity.java
@@ -1,6 +1,7 @@
 package dk.itu.info.app;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 
 public class InfoatITUAppActivity extends Activity {
@@ -9,5 +10,6 @@ public class InfoatITUAppActivity extends Activity {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
+        startActivity(new Intent(this, AccountList.class));
     }
 }
