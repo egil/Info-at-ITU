b7a04a29e362f6f11fb61476bad6625baf364a2b
Egil Hansen <egil@assimilated.dk>
2012-03-15 16:14:16 +0100

    blah

--- android/gen/dk/itu/info/app/R.java
+++ android/gen/dk/itu/info/app/R.java
@@ -14,7 +14,9 @@ public final class R {
         public static final int ic_launcher=0x7f020000;
     }
     public static final class id {
-        public static final int button1=0x7f050000;
+        public static final int messageText=0x7f050001;
+        public static final int relativeLayout1=0x7f050000;
+        public static final int signInOutButton=0x7f050002;
     }
     public static final class layout {
         public static final int app_info=0x7f030000;
@@ -22,8 +24,10 @@ public final class R {
         public static final int main=0x7f030002;
     }
     public static final class string {
-        public static final int app_name=0x7f040001;
-        public static final int sign_in=0x7f040002;
-        public static final int welcomemsg=0x7f040000;
+        public static final int app_name=0x7f040000;
+        public static final int sign_in=0x7f040003;
+        public static final int sign_out=0x7f040004;
+        public static final int tracking_you_msg=0x7f040002;
+        public static final int welcome_msg=0x7f040001;
     }
 }
--- android/src/dk/itu/info/app/InfoatITUAppActivity.java
+++ android/src/dk/itu/info/app/InfoatITUAppActivity.java
@@ -1,32 +1,59 @@
 package dk.itu.info.app;
 
 import android.app.Activity;
+import android.bluetooth.BluetoothAdapter;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class InfoatITUAppActivity extends Activity {
-    /** Called when the activity is first created. */
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.main);
-        
-        // welcome message
-        // signin request
-        
-        // turn on gps
-        // start tracking service
-        
-        /** 
-         * User notification 
-         * Use http://developer.android.com/guide/topics/ui/notifiers/notifications.html
-         * 1z
-         */
-        
-    }
-    
-    public void goToAccountListActivity(View view) {
-    	startActivity(new Intent(this, AccountList.class));
-    }
+	private Boolean signedIn = false;
+	private String btMACAddr = null;
+
+	/** Called when the activity is first created. */
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.main);
+		
+//		BluetoothAdapter bluetoothAdapter = BluetoothAdapter.getDefaultAdapter();		
+//		if (bluetoothAdapter == null) {	
+//			// TODO: Add pretty warning and exit, bluetooth not supported by phone :( 			
+//		}
+//		btMACAddr = bluetoothAdapter.getAddress();
+	}
+
+	@Override
+	protected void onResume() {
+		super.onResume();
+
+		// welcome message
+		// signin request
+
+		// turn on gps
+		// start tracking service
+
+		/**
+		 * User notification Use
+		 * http://developer.android.com/guide/topics/ui/notifiers
+		 * /notifications.html 1z
+		 */
+	}
+
+	public void onSignInOut(View view) {
+		if (this.signedIn) {
+			Intent intent = new Intent(this, TrackingService.class);
+			startService(intent);
+		} else {
+			// sing in
+		}
+		this.signedIn = !this.signedIn;
+		Button signInOutButton = (Button) findViewById(R.id.signInOutButton);
+		TextView messageText = (TextView) findViewById(R.id.messageText);
+		signInOutButton.setText((signedIn ? R.string.sign_out : R.string.sign_in));
+		messageText.setText((signedIn ? R.string.tracking_you_msg : R.string.welcome_msg));
+	}
+
 }
